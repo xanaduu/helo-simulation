3cfd9eafbae9fc940fe3a6a1f89d3a467a3a078f
Alex <alexrminnick@gmail.com>
2018-01-12 09:41:48 -0700

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import '../styles/App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    )
+  }
+}
+
+export default App
